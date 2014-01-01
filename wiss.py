a7dd8002905783dffb4795eb9f0d0dff481bd1ea
tomhanzal <tomhanzal@seznam.cz>
2014-01-01 19:11:12 +0000

    added get_sparql_query function

--- wiss.py
+++ wiss.py
@@ -5,45 +5,49 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 app = Flask(__name__)
 
-def convert_name(name):
-    output = re.sub('\s\(\w*\)', '', name)
-    if ',' in output:
-        output = output.split(', ')
-        output = '%s %s' % (output[1], output[0])
-    return output
 
+def get_sparql_query(what, q):
+    if what == 'search':
+        sparql_query = """
+            PREFIX dc: <http://purl.org/dc/elements/1.1/>
+            PREFIX ore: <http://www.openarchives.org/ore/terms/>
+            PREFIX edm: <http://www.europeana.eu/schemas/edm/>
 
-@app.route('/')
-def homepage():
-    return render_template('homepage.html')
+            SELECT ?title ?content_type ?content_provider ?link ?image ?object (GROUP_CONCAT(?desc ; SEPARATOR = " ") AS ?description) (GROUP_CONCAT(?au ; SEPARATOR = ";") AS ?author)
+            WHERE {
+              ?proxy dc:subject "%s";
+                dc:title ?title ;
+                dc:creator ?au ;
+                ore:proxyIn [edm:isShownAt ?link; edm:object ?image; edm:dataProvider ?content_provider] ;
+                ore:proxyFor ?object ;
+                edm:type ?content_type ;
+                dc:description ?desc .
+            }
+            GROUP BY ?object ?title ?content_type ?content_provider ?link ?image ?author
+            LIMIT 100
+        """ % q
 
-@app.route('/search')
-def search():
-    query = request.args.get('q', '', type=str)
+    elif what == 'search_author':
+        sparql_query = """
+            PREFIX dc: <http://purl.org/dc/elements/1.1/>
+            PREFIX ore: <http://www.openarchives.org/ore/terms/>
+            PREFIX edm: <http://www.europeana.eu/schemas/edm/>
 
-    sparql = SPARQLWrapper("http://europeana.ontotext.com/sparql")
-    sparql.setQuery("""
-        PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX ore: <http://www.openarchives.org/ore/terms/>
-        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+            SELECT ?title ?author ?content_type ?content_provider ?link ?image ?object (GROUP_CONCAT(?desc ; SEPARATOR = " ") AS ?description)
+            WHERE {
+              ?proxy dc:creator "%s", ?author ;
+                dc:title ?title ;
+                dc:description ?desc ;
+                ore:proxyIn [edm:isShownAt ?link; edm:object ?image; edm:dataProvider ?content_provider] ;
+                ore:proxyFor ?object ;
+                edm:type ?content_type .
+            }
+            GROUP BY ?object ?title ?content_type ?content_provider ?link ?image ?author
+            LIMIT 40
+        """ % q
 
-        SELECT ?title ?content_type ?content_provider ?link ?image ?object (GROUP_CONCAT(?desc ; SEPARATOR = " ") AS ?description) (GROUP_CONCAT(?au ; SEPARATOR = ";") AS ?author)
-        WHERE {
-          ?proxy dc:subject "%s";
-            dc:title ?title ;
-            dc:creator ?au ;
-            ore:proxyIn [edm:isShownAt ?link; edm:object ?image; edm:dataProvider ?content_provider] ;
-            ore:proxyFor ?object ;
-            edm:type ?content_type ;
-            dc:description ?desc .
-        }
-        GROUP BY ?object ?title ?content_type ?content_provider ?link ?image ?author
-        LIMIT 100
-    """ % query)
-
-    # If searching by URI...
-    if query[0:7] == "http://":
-        sparql.setQuery("""
+    elif what == 'search_uri':
+        sparql_query = """
             PREFIX dc: <http://purl.org/dc/elements/1.1/>
             PREFIX ore: <http://www.openarchives.org/ore/terms/>
             PREFIX edm: <http://www.europeana.eu/schemas/edm/>
@@ -60,7 +64,86 @@ def search():
                 edm:type ?content_type .
             }
             LIMIT 100
-        """ % query)
+        """ % q
+
+    elif what == 'get_author_info':
+        sparql_query = """
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            PREFIX dbo: <http://dbpedia.org/ontology/>
+            PREFIX yago: <http://dbpedia.org/class/yago/>
+
+            SELECT ?name ?abstract ?image ?link
+            WHERE {
+              ?person foaf:name "%s"@en ;
+                a yago:Artist109812338 ;
+                dbo:abstract ?abstract ;
+                foaf:name ?name ;
+                foaf:isPrimaryTopicOf ?link .
+              optional {?person foaf:depiction ?image . }
+              filter(lang(?abstract) = "en")
+            }
+            LIMIT 1
+        """ % q
+
+    elif what == 'get_subjects':
+        sparql_query = """
+            PREFIX dc: <http://purl.org/dc/elements/1.1/>
+            PREFIX ore: <http://www.openarchives.org/ore/terms/>
+            PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+
+            SELECT ?subject
+            WHERE {
+              {
+              ?proxy ore:proxyFor %s ;
+                dc:subject ?subject .
+              }
+              UNION {
+              ?proxy ore:proxyFor %s ;
+                edm:hasMet ?subject .
+              }
+            }
+        """ % (q, q)
+
+    elif what == 'get_gemet_label':
+        sparql_query = """
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+            SELECT ?label
+            WHERE {
+              <%s> skos:prefLabel ?label .
+              filter(lang(?label) = "en")
+            }
+        """ % q
+
+    return sparql_query
+        
+
+
+def convert_name(name):
+    output = re.sub('\s\(\w*\)', '', name)
+    if ',' in output:
+        output = output.split(', ')
+        output = '%s %s' % (output[1], output[0])
+    return output
+
+
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q', '', type=str)
+
+    sparql = SPARQLWrapper("http://europeana.ontotext.com/sparql")
+    sparql.setQuery(get_sparql_query('search', query))
+
+    # If searching by URI...
+    if query[0:7] == "http://":
+        sparql.setQuery(get_sparql_query('search_uri', query))
 
     sparql.setReturnFormat(JSON)
 
@@ -99,23 +182,7 @@ def search_author():
     query = request.args.get('q', '', type=str)
 
     sparql = SPARQLWrapper("http://europeana.ontotext.com/sparql")
-    sparql.setQuery("""
-        PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX ore: <http://www.openarchives.org/ore/terms/>
-        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
-
-        SELECT ?title ?author ?content_type ?content_provider ?link ?image ?object (GROUP_CONCAT(?desc ; SEPARATOR = " ") AS ?description)
-        WHERE {
-          ?proxy dc:creator "%s", ?author ;
-            dc:title ?title ;
-            dc:description ?desc ;
-            ore:proxyIn [edm:isShownAt ?link; edm:object ?image; edm:dataProvider ?content_provider] ;
-            ore:proxyFor ?object ;
-            edm:type ?content_type .
-        }
-        GROUP BY ?object ?title ?content_type ?content_provider ?link ?image ?author
-        LIMIT 40
-    """ % query)
+    sparql.setQuery(get_sparql_query('search_author', query))
 
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
@@ -150,24 +217,7 @@ def author_info():
     author = convert_name(author)
 
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    sparql.setQuery("""
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX yago: <http://dbpedia.org/class/yago/>
-
-        SELECT ?name ?abstract ?image ?link
-        WHERE {
-          ?person foaf:name "%s"@en ;
-            a yago:Artist109812338 ;
-            dbo:abstract ?abstract ;
-            foaf:name ?name ;
-            foaf:isPrimaryTopicOf ?link .
-          optional {?person foaf:depiction ?image . }
-          filter(lang(?abstract) = "en")
-        }
-        LIMIT 1
-    """ % author)
+    sparql.setQuery(get_sparql_query('get_author_info', author))
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
@@ -185,29 +235,14 @@ def author_info():
 
     return jsonify(author_info)
 
+
 @app.route('/s')
 def list_subjects():
     obj = request.args.get('obj', '', type=str)
     obj = "<%s>" % obj
 
     sparql = SPARQLWrapper("http://europeana.ontotext.com/sparql")
-    sparql.setQuery("""
-        PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX ore: <http://www.openarchives.org/ore/terms/>
-        PREFIX edm: <http://www.europeana.eu/schemas/edm/>
-
-        SELECT ?subject
-        WHERE {
-          {
-          ?proxy ore:proxyFor %s ;
-            dc:subject ?subject .
-          }
-          UNION {
-          ?proxy ore:proxyFor %s ;
-            edm:hasMet ?subject .
-          }
-        }
-    """ % (obj, obj))
+    sparql.setQuery(get_sparql_query('get_subjects', obj))
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
@@ -221,16 +256,7 @@ def list_subjects():
         if subject[0:42] == "http://www.eionet.europa.eu/gemet/concept/":
 
             sparql = SPARQLWrapper("http://semantic.eea.europa.eu/sparql")
-            sparql.setQuery("""
-                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-
-                SELECT ?label
-                WHERE {
-                  <%s> skos:prefLabel ?label .
-                  filter(lang(?label) = "en")
-                }
-            """ % subject)
+            sparql.setQuery(get_sparql_query('get_gemet_label', subject))
             sparql.setReturnFormat(JSON)
             thes_result = sparql.query().convert()
 
